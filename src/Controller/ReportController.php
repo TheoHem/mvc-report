3d3165396a11789cf1e2d60f281383afe042d590
Theo Hembäck <theohemback@hotmail.com>
2022-04-07 17:39:30 +0100

    Kursmoment 1 färdigt

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-//use function Theo\Functions\url;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
